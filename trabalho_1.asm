007e9a0eb2e59fdc40dbad84a065c39c9983cd40
liviafonseca <liviagcf@gmail.com>
2018-10-02 20:32:52 -0300

    thresholding

--- trabalho_1.asm
+++ trabalho_1.asm
@@ -1,13 +1,19 @@
 .data
 file: .asciiz "lena.bmp"
 pixel: .word 0
-tamx: .word
-tamy: .word
-header: .space 56
-offs: .word
-byte: .byte
+tamx: .word 0
+tamy: .word 0
+offs: .word 0
+byte: .byte 0
+tam_blur: .word 1
+img: .word 0x10040000
 
 menu: .asciiz "MENU\n1) Abrir imagem\n2) Blur effect\n3) Edge Extractor\n4) Thresholding\n5) Sair\nEscolha uma opcao:"
+threshold: .asciiz "\n\nDigite os intervalos de cores: \nR_max, G_max e B_max \nR_min, G_min e B_min\n"
+
+blur: .asciiz "Digite o tamanho do kernel do blur: "
+.align 2
+header: .space 56
 
 .text
 
@@ -77,6 +83,10 @@ addi $t1,$t1,1
 bge $t1,$s2,abre    # verifica se chegou no fim da linha
 bne $v0,$zero,loop  # verifica se a imagem terminou
 
+li   $v0, 16       # system call for close file
+move $a0, $s0      # file descriptor to close
+syscall            # close file
+
 
 j abre_menu   # volta para o menu
 
@@ -93,7 +103,7 @@ li $v0,5
 syscall
 
 # determina qual funcao realizar
-la $t1,($v0)
+move $t1,$v0
 beq $t1,1,abrir
 beq $t1,2,blur_effect
 beq $t1,3,edge_extractor
@@ -116,7 +126,77 @@ edge_extractor:
 ################################################################################
 thresholding:
 
+la $a0,threshold
+li $v0,4
+syscall
 
+lw $s2,tamx
+lw $s3,tamy
+mul $s1,$s2,$s3
+mul $s1,$s1,4
+
+# Valor maximo
+li $v0,5
+syscall
+move $s4,$v0
+
+li $v0,5
+syscall
+move $s5,$v0
+
+li $v0,5
+syscall
+move $s6,$v0
+
+
+# coloca em um unico registrador
+mul $s4,$s4,0x00010000
+mul $s5,$s5,0x0000100
+add $s4,$s4,$s5
+add $s4,$s4,$s6
+
+
+# Valor minimo
+li $v0,5
+syscall
+move $s7,$v0
+
+li $v0,5
+syscall
+move $s5,$v0
+
+li $v0,5
+syscall
+move $s6,$v0
+
+# coloca em um unico registrador
+mul $s7,$s7,0x00010000
+mul $s5,$s5,0x0000100
+add $s7,$s7,$s5
+add $s7,$s7,$s6
+
+
+lw $t0,img
+add $s1,$s1,$t0
+li $t4,0x00ffffff
+
+
+loop_thresh:
+lw $t2, ($t0)
+bge $t2,$s4,zero	# conefere se e menor que o maximo
+bge $s7,$t2,zero	#confere se e maior que o minimo
+
+sw $t4,($t0)
+addi $t0,$t0,4
+bge $s1,$t0,loop_thresh
+
+zero:
+sw $zero,($t0)
+
+addi $t0,$t0,4
+bge $s1,$t0,loop_thresh
+
+j abre_menu   # volta para o menu
 
 
 
